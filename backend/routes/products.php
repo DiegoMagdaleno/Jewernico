c2800d43f79b762cc6e8824048bee2c94832eb5a
Diego Magdaleno <diegomagdaleno@protonmail.com>
2023-11-30 10:17:11 -0600

    [Frontend] Iniciar a trabajar en el panel de admin

---
+++ backend/routes/products.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once 'vendor/autoload.php';
+require_once 'util.php';
+
+Flight::route("GET /products", function () {
+    $db = Flight::db();
+    $query = $db->prepare("SELECT * FROM producto");
+    $query->execute();
+    $products = $query->fetchAll();
+    Flight::json($products);
+});
+
+Flight::route("GET /products/@id", function ($id) {
+    $db = Flight::db();
+    $query = $db->prepare("SELECT * FROM producto WHERE Id = :id");
+    $query->execute(
+        array(
+            ":id" => $id
+        )
+    );
+    $product = $query->fetch();
+    Flight::json($product);
+});
+
+Flight::route("POST /products", function () {
+    $token = validateToken();
+
+    if (!$token) {
+        Flight::json(
+            array(
+                "status" => 403,
+                "message" => "No autorizado"
+            ),
+            403
+        );
+    } else {
+        if ($token->data->nivelPermisos < 1) {
+            Flight::json(
+                array(
+                    "status" => 403,
+                    "message" => "No autorizado"
+                ),
+                403
+            );
+        } else {
+            $data = Flight::request()->data->getData();
+            $files = Flight::request()->files->getData();
+            $db = Flight::db();
+
+            if (isset($data["descripcion"])) {
+                $data["descripcion"] = htmlspecialchars($data["descripcion"]);
+            } else {
+                $data["descripcion"] = "";
+            }
+
+            $query = $db->prepare("INSERT INTO producto (Nombre, IdCategoria, IdMaterial, Descripcion, Precio, Stock) VALUES (:nombre, :idCategoria, :idMaterial, :descripcion, :precio, :stock)");
+            $query->execute(
+                array(
+                    ":nombre" => $data["nombre"],
+                    ":idCategoria" => $data["idCategoria"],
+                    ":idMaterial" => $data["idMaterial"],
+                    ":descripcion" => $data["descripcion"],
+                    ":precio" => $data["precio"],
+                    ":stock" => $data["stock"]
+                )
+            );
+            Flight::json(
+                array(
+                    "status" => 200,
+                    "message" => "Producto agregado"
+                ),
+                200
+            );
+        }
+    }
+});
---
+++ frontend/admin/js/state.js
@@ -0,0 +1,3 @@
+const STATE = {
+    currentView: 'products',
+}
