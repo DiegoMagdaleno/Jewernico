f0792d61867a244422c227b11c32c83502f00f0d
Diego Magdaleno <diegomagdaleno@protonmail.com>
2023-12-01 19:47:50 -0600

    [Backend] Ahora es posible refrescar el Token

--- backend/routes/auth/refresh.php
+++ backend/routes/auth/refresh.php
@@ -1,5 +1,46 @@
 <?php
 
-Flight::route("POST /refresh_token", function(){
-    
+require_once "vendor/autoload.php";
+require_once "tokens.php";
+
+use Firebase\JWT\ExpiredException;
+
+Flight::route("POST /refresh_token", function () {
+    $data = Flight::request()->data->getData();
+    $tokenCookie = Flight::request()->cookies->token;
+
+    try {
+        $token = checkToken($tokenCookie, "ALGUNA_LLAVE_SECRETA");
+        // Nice! lets get the refresh token
+        $refreshTokenReq = $data["refreshToken"];
+        try {
+            checkToken($refreshTokenReq, "ALGUNA_LLAVE_SECRETA");
+            // Perfect lets create a new token
+            $newToken = generateToken("ALGUNA_LLAVE_SECRETA", $token->data, strtotime("now") + 3600);
+            Flight::json(
+                array(
+                    "token" => $newToken
+                )
+            );
+        } catch (ExpiredException $e) {
+            Flight::json(
+                array(
+                    "status" => 401,
+                    "message" => "Token expirado"
+                ),
+                401
+            );
+        }
+
+    } catch (ExpiredException $e) {
+        Flight::json(
+            array(
+                "status" => 401,
+                "message" => "Token expirado"
+            ),
+            401
+        );
+    }
 });
+
+?>
