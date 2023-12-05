ef8b09d456ab48ffe94bad5a18dc5d487ee27c2e
Diego Magdaleno <diegomagdaleno@protonmail.com>
2023-12-04 19:28:27 -0600

    [Backend] Tambien regresar el token en caso de pregunta

--- backend/routes/question.php
+++ backend/routes/question.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once('vendor/autoload.php');
+require_once('tokens.php');
 
 Flight::route('GET /questions', function () {
     $db = Flight::db();
@@ -51,12 +52,39 @@ Flight::route("GET /question/validate", function () {
 
     $respuesta = $query->fetchAll();
     if (count($respuesta) > 0) {
-        // Check if the answer is equal to the data answer
+
         if ($respuesta[0]["Respuesta"] == $data["Respuesta"]) {
+            $userQuery = $db->prepare("SELECT * FROM usuario WHERE Id = :usuario");
+            $userQuery->execute(
+                array(
+                    ":usuario" => $usuario
+                )
+            );
+            $usuario = $userQuery->fetch();
+            $dataToken = array(
+                "id" => $usuario["Id"],
+                "nombre" => $usuario["Nombre"],
+                "apellidoMaterno" => $usuario["ApellidoMaterno"],
+                "apellidoPaterno" => $usuario["ApellidoPaterno"],
+                "nivelPermisos" => $usuario["NivelPermisos"],
+            );
+
+            $refreshToken = array(
+                "id" => $usuario["Id"],
+                "nivelPermisos" => $usuario["NivelPermisos"],
+            );
+
+            $now = time();
+
+            $jwt = generateToken("ALGUNA_CLAVE_SECRETA", $dataToken, $now + 60 * 60 * 24);
+            $jwtRefresh = generateToken("ALGUNA_CLAVE_SECRETA", $refreshToken, $now + 604800);
+
             Flight::json(
                 array(
                     "status" => 200,
-                    "message" => "Respuesta correcta"
+                    "message" => "Respuesta correcta",
+                    "token" => $jwt,
+                    "RefreshToken" => $jwtRefresh
                 )
             );
         } else {
