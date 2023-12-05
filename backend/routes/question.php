5be95da8503137d2e2c498174c2cf835e61b7f7e
Diego Magdaleno <diegomagdaleno@protonmail.com>
2023-12-04 19:22:45 -0600

    [Backend] Añadir codigo de validacion de preguntas

--- backend/routes/question.php
+++ backend/routes/question.php
@@ -2,9 +2,70 @@
 
 require_once('vendor/autoload.php');
 
-Flight::route('GET /questions', function(){
+Flight::route('GET /questions', function () {
     $db = Flight::db();
     $query = $db->query('SELECT * FROM pregunta');
     $questions = $query->fetchAll();
     Flight::json($questions);
+});
+
+Flight::route("POST /questions/answers", function () {
+    $db = Flight::db();
+    $data = $db->request()->data->getData();
+
+    $usuario = $data["IdUsuario"];
+    $pregunta = $data["IdPregunta"];
+    $respuesta = $data["Respuesta"];
+
+    $query = $db->prepare("INSERT INTO responder (IdUsuario, IdPregunta, Respuesta) VALUES (:usuario, :pregunta, :respuesta)");
+    $query->execute(
+        array(
+            ":usuario" => $usuario,
+            ":pregunta" => $pregunta,
+            ":respuesta" => $respuesta
+        )
+    );
+
+    Flight::json(
+        array(
+            "status" => 200,
+            "message" => "Respuesta enviada"
+        )
+    );
+});
+
+Flight::route("GET /question/validate", function () {
+    $db = Flight::db();
+    $data = Flight::request()->data->getData();
+
+    $usuario = $data["IdUsuario"];
+    $pregunta = $data["IdPregunta"];
+
+    $query = $db->prepare("SELECT * FROM responder WHERE IdUsuario = :usuario AND IdPregunta = :pregunta");
+    $db->execute(
+        array(
+            ":usuario" => $usuario,
+            ":pregunta" => $pregunta
+        )
+    );
+
+    $respuesta = $query->fetchAll();
+    if (count($respuesta) > 0) {
+        // Check if the answer is equal to the data answer
+        if ($respuesta[0]["Respuesta"] == $data["Respuesta"]) {
+            Flight::json(
+                array(
+                    "status" => 200,
+                    "message" => "Respuesta correcta"
+                )
+            );
+        } else {
+            Flight::json(
+                array(
+                    "status" => 403,
+                    "message" => "Respuesta incorrecta"
+                )
+            );
+        }
+    }
 });
