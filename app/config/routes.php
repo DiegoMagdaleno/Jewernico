68bec67db8a65ee7ef43320dd3d68f7586315ffa
Diego Magdaleno <diegomagdaleno@protonmail.com>
2023-12-07 20:27:41 -0600

    [Root] Inicializar nuevo esqueleto

---
+++ app/config/routes.php
@@ -0,0 +1,17 @@
+<?php
+
+// -------------------------------------------------- //
+// ROUTES
+// -------------------------------------------------- //
+
+Flight::route("/", ["\Acme\Demo\Controller\Demo", "index"]);
+Flight::route("/api/message", ["\Acme\Demo\Controller\API", "message"]);
+Flight::route("/hello", ["\Acme\Demo\Controller\Demo", "hello"]);
+
+// -------------------------------------------------- //
+// MAPPINGS
+// -------------------------------------------------- //
+
+Flight::map("notFound", fn() => Flight::view()->display("404.twig", []));
+
+Flight::map("error", fn(Throwable $exception) => $logger->error($exception));
