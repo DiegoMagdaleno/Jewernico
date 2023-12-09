c3a63217993ed5d19d5c802bbf4ac769db702b85
Diego Magdaleno <diegomagdaleno@protonmail.com>
2023-12-09 17:50:09 -0600

    [Backend+Frontend] Panel de productos restaurado

--- src/Acme/Jewernico/Controller/Admin.php
+++ src/Acme/Jewernico/Controller/Admin.php
@@ -8,6 +8,12 @@ class Admin {
     public static function load() {
         Flight::view()->display("admin.twig", []);
     }
+
+    public static function loadProducts() {
+        $products = \Acme\Jewernico\Command\Database::getProducts();
+
+        Flight::view()->display("admin_products.twig", ["productos"=> $products]);
+    }
 }
 
 
