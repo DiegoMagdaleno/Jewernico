035898b2bc77dcaf7f023259673080c3890e3441
Diego Magdaleno <diegomagdaleno@protonmail.com>
2023-12-09 13:34:28 -0600

    [Frontend] Ahora es posible ver las imagenes de los productos

---
+++ src/Acme/Jewernico/Command/Database.php
@@ -0,0 +1,39 @@
+<?php
+namespace Acme\Jewernico\Command;
+
+use Flight;
+
+class Database
+{
+    public static function getCategories()
+    {
+        $db = Flight::db();
+        $categories = $db->query("SELECT * FROM categoria")->fetchAll();
+        return $categories;
+    }
+
+    public static function getMaterials()
+    {
+
+        $db = Flight::db();
+        $materials = $db->query("SELECT * FROM material")->fetchAll();
+        return $materials;
+    }
+
+    public static function getProducts()
+    {
+        $db = Flight::db();
+        $products = $db->query("SELECT * FROM info_producto")->fetchAll();
+        return $products;
+    }
+
+    public static function getProduct($id)
+    {
+        $db = Flight::db();
+        $product = $db->prepare("SELECT * FROM info_producto WHERE Id = :id");
+        $product->execute(array(":id" => $id));
+        $product = $product->fetch();
+        return $product;
+    }
+}
+?>
