03cf81ca8b5cb8a7ab53b51c2a9d9b18613c1471
Diego Magdaleno <diegomagdaleno@protonmail.com>
2023-12-11 22:35:08 -0600

    [Frontend+Backend] Carrito totalmente funcional

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
 module.exports = {
   content: [
     './app/resources/views/**/*.twig',
@@ -8,7 +9,23 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('daisyui')
+    require('daisyui'),
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.arrow-hide': {
+          '-moz-appearance': 'textfield!important',
+          '&::-webkit-inner-spin-button': {
+            '-webkit-appearance': 'none',
+            'margin': 0,
+          },
+          '&::-webkit-outer-spin-button': {
+            '-webkit-appearance': 'none',
+            'margin': 0
+          },
+        }
+      }
+      )
+    })
   ],
   daisyui: {
     themes: ["cupcake"],
