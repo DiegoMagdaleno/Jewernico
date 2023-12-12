dae5cea54e853e07c1a9ca11c26f309959c68351
Diego Magdaleno <diegomagdaleno@protonmail.com>
2023-12-12 08:19:19 -0600

    [Backend] Medidas extras de seguridad

--- src/Acme/Jewernico/Controller/Admin.php
+++ src/Acme/Jewernico/Controller/Admin.php
@@ -6,22 +6,38 @@ use Flight;
 
 class Admin {
     public static function load() {
+        if (!isset($_SESSION["user"]) || $_SESSION["user"]->getPermissionLevel() < 1) {
+            Flight::redirect("/");
+        }
+
         Flight::view()->display("admin.twig", []);
     }
 
     public static function loadProducts() {
+        if (!isset($_SESSION["user"]) || $_SESSION["user"]->getPermissionLevel() < 1) {
+            Flight::redirect("/");
+        }
+
         $products = \Acme\Jewernico\Command\Database::getProducts();
 
         Flight::view()->display("admin_products.twig", ["productos"=> $products]);
     }
 
     public static function addProduct() {
+        if (!isset($_SESSION["user"]) || $_SESSION["user"]->getPermissionLevel() < 1) {
+            Flight::redirect("/");
+        }
+
         $categories = \Acme\Jewernico\Command\Database::getCategories();
         $materials = \Acme\Jewernico\Command\Database::getMaterials();
         Flight::view()->display("admin_add_product.twig", ["categories" => $categories, "materials" => $materials]);
     }
 
     public static function editProduct($id) {
+        if (!isset($_SESSION["user"]) || $_SESSION["user"]->getPermissionLevel() < 1) {
+            Flight::redirect("/");
+        }
+
         $product = \Acme\Jewernico\Command\Database::getProduct($id);
         $categories = \Acme\Jewernico\Command\Database::getCategories();
         $materials = \Acme\Jewernico\Command\Database::getMaterials();
--- src/Acme/Jewernico/Controller/Cart.php
+++ src/Acme/Jewernico/Controller/Cart.php
@@ -6,6 +6,10 @@ use Flight;
 
 class Cart {
     public static function load(){
+        if (!isset($_SESSION["user"])) {
+            Flight::redirect("/login");
+        }
+
         $cart = \Acme\Jewernico\Command\Database::getCart($_SESSION["user"]->getId());
         
         Flight::view()->display("cart.twig", ["productos" => $cart]);
